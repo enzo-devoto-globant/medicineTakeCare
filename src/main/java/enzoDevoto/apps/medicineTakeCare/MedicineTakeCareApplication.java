443a153b8070106ef3ea06586bc57900654fab8f
enzo devoto <enzo.devoto@globant.com>
2021-12-04 14:58:28 -0300

    Initial commit.

--- src/main/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplication.java
+++ src/main/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplication.java
@@ -9,5 +9,5 @@ public class MedicineTakeCareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedicineTakeCareApplication.class, args);
 	}
-
+//This is a comment line to test the github a local repository sync.
 }
