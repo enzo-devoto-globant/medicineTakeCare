f4ee0941c32736b5026c4eea0303c357cf62a4c6
enzo devoto <enzo.devoto@globant.com>
2021-12-04 15:15:53 -0300

    Inyecting Mockito dependecies for tests.

--- src/test/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplicationTests.java
+++ src/test/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplicationTests.java
@@ -1,10 +1,12 @@
 package enzoDevoto.apps.medicineTakeCare;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MedicineTakeCareApplicationTests {
+	@Mock private MedicineTakeCareApplication medicineTakeCareApplication;
 
 	@Test
 	void contextLoads() {
