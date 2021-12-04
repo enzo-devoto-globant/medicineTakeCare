42231f0e81292094963b29bd938c63ea82831e81
enzo devoto <enzo.devoto@globant.com>
2021-12-04 14:51:15 -0300

    Initial Commit.

---
+++ src/main/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplication.java
@@ -0,0 +1,13 @@
+package enzoDevoto.apps.medicineTakeCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineTakeCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineTakeCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enzoDevoto/apps/medicineTakeCare/MedicineTakeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package enzoDevoto.apps.medicineTakeCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineTakeCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
